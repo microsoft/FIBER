e687f41afed330b7b3567879fa5861fc08ed14f0
Taro Makino <taro@nyu.edu>
2022-12-22 17:57:44 -0800

    Minor changes

--- coarse_grained/fiber/modules/fiber_module.py
+++ coarse_grained/fiber/modules/fiber_module.py
@@ -164,20 +164,6 @@ class FIBERTransformerSS(pl.LightningModule):
 
         hs = self.hparams.config["hidden_size"]
 
-        # ===================== Downstream ===================== #
-        exclude_keys = ["image_queue", "text_queue", "queue_ptr", "queue_total", "image_input_queue", "text_input_queue",
-            "text_input_mask_queue"]
-        if self.hparams.config["load_path"] != "" and not self.hparams.config["test_only"]:
-            ckpt = torch.load(self.hparams.config["load_path"], map_location="cpu")
-            state_dict = ckpt["state_dict"]
-            for key in exclude_keys:
-                if key in state_dict:
-                    state_dict.pop(key)
-            state_dict = swin_adapt_position_encoding(
-                state_dict, before=config["resolution_before"], after=resolution_after
-            )
-            self.load_state_dict(state_dict, strict=False)
-
         if self.hparams.config["loss_names"]["vqa"] > 0:
             vs = self.hparams.config["vqav2_label_size"]
             self.vqa_classifier = nn.Sequential(
@@ -213,14 +199,18 @@ class FIBERTransformerSS(pl.LightningModule):
         fiber_utils.set_metrics(self)
         self.current_tasks = list()
 
-        # ===================== load downstream (test_only) ======================
-
-        if self.hparams.config["load_path"] != "" and self.hparams.config["test_only"]:
+        exclude_keys = ["image_queue", "text_queue", "queue_ptr", "queue_total", "image_input_queue", "text_input_queue",
+            "text_input_mask_queue"]
+        if self.hparams.config["load_path"] != "":
             ckpt = torch.load(self.hparams.config["load_path"], map_location="cpu")
             state_dict = ckpt["state_dict"]
             for key in exclude_keys:
                 if key in state_dict:
                     state_dict.pop(key)
+            if not self.hparams.config["test_only"]:
+                state_dict = swin_adapt_position_encoding(
+                    state_dict, before=config["resolution_before"], after=resolution_after
+                )
             self.load_state_dict(state_dict, strict=False)
 
     @torch.no_grad()
